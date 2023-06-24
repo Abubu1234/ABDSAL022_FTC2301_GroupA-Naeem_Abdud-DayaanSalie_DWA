0d23bbc88003239b42eb3d2023e6682d724f96bd
Abubu1234 <abduddayaansalie@gmail.com>
2023-06-24 04:45:31 +0200

    completed challenge 10

--- DWA 10/js/scripts.js
+++ DWA 10/js/scripts.js
@@ -1,15 +1,18 @@
-const MAX_NUMBER= 5
-const MIN_NUMBER= -5
-const STEP_AMOUNT = 5;
+const MAX_NUMBER= 10
+const MIN_NUMBER= -10
+const STEP_AMOUNT = 1;
+const RESET_AMOUNT = 0
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="substract"]')
 const add = document.querySelector('[data-key="add"]')
+const reset = document.querySelector('[data-key="reset-button"]')
+const resetOverlay = document.querySelector('[data-key="reset-overlay"]')
 
 
 const subtractHandler = () => {
-    const newValue = parseInt(number.value) - STEP_AMOUNT
-    number.value= newValue;
+    const newValue = parseInt(number.value) - STEP_AMOUNT   
+    number.value = newValue;
 
     if (add.disabled === true){
         add.disabled = false
@@ -23,7 +26,7 @@ const subtractHandler = () => {
 
 const addHandler = () => {
     const newValue = parseInt(number.value) + STEP_AMOUNT
-    number.value= newValue;
+    number.value = newValue;
 
     if (subtract.disabled === true) {
         subtract.disabled = false
@@ -33,6 +36,12 @@ const addHandler = () => {
         add.disabled = true
     }
 }
+const resetHandler = () =>{
+    number.value = 0;     
 
+    resetOverlay.show();
+}
+reset.addEventListener('click', resetHandler)
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
+
