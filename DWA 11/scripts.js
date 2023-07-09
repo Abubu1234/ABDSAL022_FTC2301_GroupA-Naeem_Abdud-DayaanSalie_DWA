482366ee115e1cd5b22b8c449c777c92c1d4977e
Abubu1234 <abduddayaansalie@gmail.com>
2023-07-09 12:48:10 +0200

    Moved files into appropriate folders

--- DWA 11/scripts.js
+++ DWA 11/scripts.js
@@ -1,65 +1,153 @@
-// Action types
-const ADD = 'ADD';
-const SUBTRACT = 'SUBTRACT';
-const RESET = 'RESET';
+/**
+ * The initial state of the application
+ * 
+ * @typedef {Object} InitialState
+ * @property {number} count - The count value
+ */
 
-// Reducer function
-function counterReducer(state = 0, action) {
+/**
+ * The action type for the reducer
+ * 
+ * @typedef {('ADD' | 'SUBTRACT' | 'RESET')} ActionType
+ */
+
+/**
+ * The reducer function that handles state updates.
+ *
+ * @typedef {function} Reducer
+ * @param {InitialState} state 
+ * @param {Object} action 
+ * @param {ActionType} action.type
+ * @returns {InitialState} 
+ */
+
+/**
+ * The store object that holds the state and manages state updates.
+ *
+ * @typedef {Object} Store
+ * @property {InitialState} state 
+ * @property {Array<function>} listeners 
+ * @property {function} getState 
+ * @property {function} dispatch 
+ * @property {function} subscribe
+ */
+
+/**
+ * Initialises the HTML elements used in the application
+ * 
+ * @type {HtmlElements}
+ */
+const html = {
+  header: {
+    settings: document.querySelector('[data-header-settings]'),
+  },
+  settings: {
+    overlay: document.querySelector('[data-settings-overlay]'),
+    form: document.querySelector('[data-settings-form]'),
+  },
+  key: {
+    number: document.querySelector('[data-key="number"]'),
+    subtract: document.querySelector('[data-key="subtract"]'),
+    add: document.querySelector('[data-key="add"]'),
+    reset: document.querySelector('[data-key="reset"]'),
+  },
+  reset: {
+    overlay: document.querySelector('[data-reset-overlay]'),
+  },
+};
+
+/**
+ * The initial state of the application
+ * 
+ * @type {InitialState}
+ */
+const initialState = {
+  count: 1,
+};
+
+/**
+ * The reducer function that handles state updates
+ * 
+ * @type {Reducer} 
+ */
+function reducer(state, action) {
   switch (action.type) {
-    case ADD:
-      return state + 1;
-    case SUBTRACT:
-      return state - 1;
-    case RESET:
-      return 0;
+    case 'ADD':
+      return {
+        count: state.count + 1,
+      };
+    case 'SUBTRACT':
+      return {
+        count: state.count - 1,
+      };
+    case 'RESET':
+      return {
+        count: 0,
+      };
     default:
       return state;
   }
 }
 
-// Store
-class Store {
-  constructor(reducer) {
-    this.state = undefined;
-    this.reducer = reducer;
-    this.subscriptions = [];
-  }
-
+/**
+ * The store object that holds the state and manages state updates
+ * 
+ * @type {Store}
+ */
+const store = {
+  state: initialState,
+  listeners: [],
+  /**
+   * Gets the current state
+   * @returns {InitialState}
+   */
   getState() {
     return this.state;
-  }
-
+  },
+  /**
+   * Dispatches an action and updates the state
+   * @param {Object} action 
+   */
   dispatch(action) {
-    this.state = this.reducer(this.state, action);
-    this.subscriptions.forEach(subscription => {
-      subscription(this.state);
-    });
-  }
+    this.state = reducer(this.state, action);
+    this.listeners.forEach((listener) => listener(this.state));
+  },
+  /**
+   * Subscribes a listener function to the store for state changes
+   * @param {function} listener 
+   */
+  subscribe(listener) {
+    this.listeners.push(listener);
+  },
+};
 
-  subscribe(callback) {
-    this.subscriptions.push(callback);
-  }
-}
+/**
+ * Logs the new state to the console
+ * @param {InitialState} state
+ */
+store.subscribe((state) => {
+  // eslint-disable-next-line no-console
+  console.log('New state:', state);
+});
 
-// Create store instance
-const store = new Store(counterReducer);
+/**
+ * Event listener for the subtract key
+ */
+html.key.subtract.addEventListener('click', () => {
+  store.dispatch({ type: 'SUBTRACT' });
+});
 
-// Scenario 1: Logging initial state
-console.log('Scenario 1: Logging initial state');
-console.log(store.getState()); // Expected: 0
+/**
+ * Event listener for the add key
+ */
+html.key.add.addEventListener('click', () => {
+  store.dispatch({ type: 'ADD' })
+  ;
+});
 
-// Scenario 2: Increment the counter by one
-console.log('\nScenario 2: Increment the counter by one');
-store.dispatch({ type: ADD });
-store.dispatch({ type: ADD });
-console.log(store.getState()); // Expected: 2
-
-// Scenario 3: Decrement the counter by one
-console.log('\nScenario 3: Decrement the counter by one');
-store.dispatch({ type: SUBTRACT });
-console.log(store.getState()); // Expected: 1
-
-// Scenario 4: Resetting the Tally Counter
-console.log('\nScenario 4: Resetting the Tally Counter');
-store.dispatch({ type: RESET });
-console.log(store.getState()); // Expected: 0
+/**
+ * Event listener for the reset key
+ */
+html.key.reset.addEventListener('click', () => {
+  store.dispatch({ type: 'RESET' });
+});
